405c9cb17a0d4e3647278e64c1ffb10beee82fdb
Yaron Horsky <horsky.yaron@gmail.com>
2020-12-11 17:08:36 +0200

    added some debuging stuff and updated gitignore file

---
+++ cmake-build-debug/CMakeFiles/c__Project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c__Project.dir/MainTrain.cpp.o"
+  "CMakeFiles/c__Project.dir/SimpleAnomalyDetector.cpp.o"
+  "CMakeFiles/c__Project.dir/anomaly_detection_util.cpp.o"
+  "CMakeFiles/c__Project.dir/timeseries.cpp.o"
+  "c__Project"
+  "c__Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c__Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
